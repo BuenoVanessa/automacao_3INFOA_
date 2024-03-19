0af1462001b3e6ec294d95183868316889810d1b
Vanessa <vanessa.bueno@alunos.ifsuldeminas.edu.br>
2024-03-19 16:54:33 -0300

    Aula 5

---
+++ semana5/mouse_ex2.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+#Coloca o mouse em cima da imagem 
+pyautogui.moveTo(188, 233, duration=0.2) 
+#Clica na imagem 
+#pyautogui.click(188, 232)
+#Arrasta a imagem para a pasta 
+pyautogui.dragRel(255,0, duration=0.5)
---
+++ semana5/teclado1.py
@@ -0,0 +1,14 @@
+import pyautogui
+
+#Clica no campo usuário
+pyautogui.click(880, 393, duration= 0.2)
+#digita a matricula no campo
+pyautogui.write('2022190015', interval=0.5)
+
+#clica no campo senha 
+pyautogui.click(880, 500, duration=0.2)
+#digita a matricula no campo
+pyautogui.write('Vanessabueno!', interval=0.5)
+
+#Clica no campo acessar 
+pyautogui.click(880, 582, duration=0.2)
